c44006f2906d22273d97e5d51eec90d3b0affabe
SteelCurtain85 <michael.s.pollard@gmail.com>
2017-10-12 14:33:53 -0500

    delete db.js

--- issue-tracker-app/issue-api/routes/login.js
+++ issue-tracker-app/issue-api/routes/login.js
@@ -3,13 +3,10 @@ var router = express.Router();
 var Sequelize = require ('sequelize');
 var bcrpyt = require ('bcryptjs');
 module.exports = router;
-var db = require('../services/db');
-
 
 //const Users = db.conn.import('../models/IssueTracker_Users.js')
 
 /* GET login page. */
 router.get('/', function(req, res, next) {
-    res.render('login');
+    res.render('login', { title: 'Login' });
 });
-
